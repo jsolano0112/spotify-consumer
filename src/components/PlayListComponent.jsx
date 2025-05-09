623f7da49242e650ff09daea33b2928e49c11fb6
Juana Solano <juana.solano@correo.tdea.edu.co>
2025-05-09 18:48:44 -0500

    fix: loading time

--- src/components/PlayListComponent.jsx
+++ src/components/PlayListComponent.jsx
@@ -24,7 +24,7 @@ export default function PlayList({ setMenu }) {
       setTimeout(() => {
         setPlaylists(cards);
         setLoading(false);
-      }, 2000 );
+      }, 3000 );
     };
 
     fetchUserPlaylists();
